f063912151bca69d4dd17b9d72457773bf3091bc
Sanchit Karve <write2sanchit@gmail.com>
2012-05-12 01:38:00 -0700

    Ready to Deploy!

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ captivate-search-button-fix
 ===========================
 
 Fixes an issue where the Search key gets automatically pressed on the Samsung Captivate.
+
+TODO: Explain how the app works along with a detailed explaination of all the shell commands used.
--- src/com/sanchitkarve/CaptivateSearchFix/CaptivateSearchFixActivity.java
+++ src/com/sanchitkarve/CaptivateSearchFix/CaptivateSearchFixActivity.java
@@ -10,19 +10,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
-import android.widget.Toast;
-
+import android.widget.TextView;
 import com.stericson.RootTools.RootTools;
 
 public class CaptivateSearchFixActivity extends Activity {
 	Button btnFix;
 	Button btnUnfix;
+	TextView lblResult;
 	RadioButton optSearch;
-	RadioButton optBack;
+	/*RadioButton optBack;
 	RadioButton optHome;
-	RadioButton optMenu;
+	RadioButton optMenu;*/
 	
-    /** Called when the activity is first created. */
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,109 +34,151 @@ public class CaptivateSearchFixActivity extends Activity {
         btnUnfix = (Button)findViewById(R.id.btnUnfix);
         btnUnfix.setOnClickListener(btnUnfixOnClick);
         
+        lblResult = (TextView)findViewById(R.id.lblResult); 
+        
         optSearch = (RadioButton)findViewById(R.id.optSearch);
-        optBack = (RadioButton)findViewById(R.id.optBack);
+        /*optBack = (RadioButton)findViewById(R.id.optBack);
         optHome = (RadioButton)findViewById(R.id.optHome);
-        optMenu = (RadioButton)findViewById(R.id.optMenu);
+        optMenu = (RadioButton)findViewById(R.id.optMenu);*/
+        try
+        {
+        	if(!RootTools.isRootAvailable())
+        	{
+        		btnFix.setEnabled(false);        		
+        		btnUnfix.setEnabled(false);        		
+        		btnFix.setText("NO ROOT ACCESS");
+        		btnUnfix.setText("SUPERUSER NOT FOUND");
+        		return;
+        	}
+        	if(!RootTools.isBusyboxAvailable())
+        	{
+        		btnFix.setEnabled(false);        		
+        		btnUnfix.setEnabled(false);
+        		String msg = getString(R.string.BusyBoxFAIL);
+        		btnFix.setText(msg);
+        		btnUnfix.setText(msg);
+        		return;
+        	}
+        	if(!RootTools.isAccessGiven())
+        	{
+        		btnFix.setEnabled(false);        		
+        		btnUnfix.setEnabled(false);
+        		String msg = getString(R.string.RootPermissionFAIL);
+        		btnFix.setText(msg);
+        		btnUnfix.setText(msg);        		
+        	}
+        	
+        }
+        catch(Exception e){e.printStackTrace();}
+    }
+    
+    /**
+     * 
+     * @param mode 0 - Fix. 1 - Unfix
+     */
+    private void fixCaptivateSearchButton(int mode)
+    {
+		String result = getString(R.string.lblResult) + "\n";
+		lblResult.setText(result);
+		/*
+    	String buttonTag = "";
+		if(optSearch.isChecked()) buttonTag = getString(R.string.optSearchText);
+		else if(optHome.isChecked()) buttonTag = getString(R.string.optHomeText);
+		else if(optMenu.isChecked()) buttonTag = getString(R.string.optMenuText);
+		else if(optBack.isChecked()) buttonTag = getString(R.string.optBackText);
+		*/
+		
+		try {					
+			//get mount point
+			List<String> mntRes = RootTools.sendShell("mount | grep /system | sed -e 's/\\(.*\\) \\/system .*/\\1/'", 1000);
+			if(mntRes.size() == 2 && mntRes.get(1).equals("0"))
+			{
+				//Toast.makeText(getBaseContext(), "Success. /system = " + mntRes.get(0), Toast.LENGTH_SHORT).show();
+				result += getString(R.string.MountSuccess) + mntRes.get(0) + "\n";
+				lblResult.setText(result);
+			}
+			else
+			{
+				//Toast.makeText(getBaseContext(), "Failure. Could not find /system. Value = " + mntRes.get(0) + " Error Code = " + mntRes.get(1), Toast.LENGTH_SHORT).show();
+				result += getString(R.string.MountFail) + "\n";					
+				lblResult.setText(result);
+				return;
+			}			
+			//Mount as rewritable
+			List<String> remntRes = RootTools.sendShell("mount -o rw,remount -t yaffs2 " + mntRes.get(0) + " /system/", 1000);
+			if(remntRes.size() == 1 && remntRes.get(0).equals("0"))
+			{
+				//Toast.makeText(getBaseContext(), "Success: Mounted /system as RW.", Toast.LENGTH_SHORT).show();
+				result += getString(R.string.MountRWSuccess) + "\n";
+				lblResult.setText(result);
+			}
+			else
+			{
+				//Toast.makeText(getBaseContext(), "Failure: Could not mount /system as RW.\nError Code = " + remntRes.get(0), Toast.LENGTH_SHORT).show();
+				result += getString(R.string.MountRWFail) + "\n";
+				lblResult.setText(result);
+				return;
+			}
+
+			String shellCmd = "";
+			BufferedReader br = null;
+		    try {
+		    	String cmdFile = (mode == 0) ? "comment.txt" : "uncomment.txt";
+		        br = new BufferedReader(new InputStreamReader(getAssets().open(cmdFile)));
+		        String line = "";
+		        while((line = br.readLine()) != null)
+		        	shellCmd = line;
+		    }
+		    catch(IOException e) {
+		    	e.printStackTrace();
+		    }
+		    finally {
+		    	try {
+		    		br.close();
+		        }
+		        catch(IOException ex) {
+		        	ex.printStackTrace();
+		        }
+		   }
+			List<String> uc = RootTools.sendShell(shellCmd, 1000);
+			if(uc.size() == 1 && uc.get(0).equals("0"))				
+			{
+				//Toast.makeText(getBaseContext(), i + " = " + uc.get(i), Toast.LENGTH_SHORT).show();
+				result += getString(R.string.KLModifySuccess) + "\n";
+				lblResult.setText(result);
+			}
+			else
+			{
+				result += getString(R.string.KLModifyFail) + "\n";
+				lblResult.setText(result);
+			}
+			List<String> resetmntRes = RootTools.sendShell("mount -o ro,remount -t yaffs2 " + mntRes.get(0) + " /system/", 1000);
+			if(resetmntRes.size() == 1 && resetmntRes.get(0).equals("0"))
+			{
+				//Toast.makeText(getBaseContext(), "Success: Mounted /system as RO.", Toast.LENGTH_SHORT).show();
+				result += getString(R.string.RestoreMountSuccess) + "\n";
+				lblResult.setText(result);
+			}
+			else
+			{
+				//Toast.makeText(getBaseContext(), "Failure: Could not mount /system as RO.\nError Code = " + resetmntRes.get(0), Toast.LENGTH_SHORT).show();
+				result += getString(R.string.RestoreMountFail) + "\n";
+				lblResult.setText(result);
+				return;
+			}
+
+			result += (mode == 0) ? getString(R.string.FixSuccess) :getString(R.string.UnfixSuccess);
+			result += "\n" + getString(R.string.RebootMessage);
+			lblResult.setText(result);
+		}
+		catch(Exception e){e.printStackTrace();}
     }
     
     View.OnClickListener btnFixOnClick = new View.OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			String buttonTag = "";
-			if(optSearch.isChecked()) buttonTag = "SEARCH";
-			else if(optHome.isChecked()) buttonTag = "HOME";
-			else if(optMenu.isChecked()) buttonTag = "MENU";
-			else if(optBack.isChecked()) buttonTag = "BACK";
-			
-			try {
-			/*
-			 * TODO:
-			 * 1) Figure out how to tweak timeout and sleeptime values to get the toasts to display.
-			 * 2) See the RootTools wiki for instructions of "memory saving" method to do all superuser operations in just
-			 *    one RootTools code segment so that
-			 *    a) The superuser toast is not shown every time
-			 *    b) Hoping it has something to do with the toasts not being displayed below...and using "memory saving" method might fix it.
-			 */				
-				//get mount point
-				List<String> mntRes = RootTools.sendShell("mount | grep /system | sed -e 's/\\(.*\\) \\/system .*/\\1/'", 1000);
-				if(mntRes.size() == 2 && mntRes.get(1).equals("0"))
-				{
-					Toast.makeText(getBaseContext(), "Success. /system = " + mntRes.get(0), Toast.LENGTH_SHORT).show();
-				}
-				else
-				{
-					Toast.makeText(getBaseContext(), "Failure. Could not find /system. Value = " + mntRes.get(0) + " Error Code = " + mntRes.get(1), Toast.LENGTH_SHORT).show();
-					return;
-				}			
-				//Mount as rewritable
-				List<String> remntRes = RootTools.sendShell("mount -o rw,remount -t yaffs2 " + mntRes.get(0) + " /system/", 1000);
-				if(remntRes.size() == 1 && remntRes.get(0).equals("0"))
-				{
-					Toast.makeText(getBaseContext(), "Success: Mounted /system as RW.", Toast.LENGTH_SHORT).show();
-				}
-				else
-				{
-					Toast.makeText(getBaseContext(), "Failure: Could not mount /system as RW.\nError Code = " + remntRes.get(0), Toast.LENGTH_SHORT).show();
-					return;
-				}
-				//String commentCmd = "find /system/usr/keylayout/ -type f \\( -name '*.kl' ! -name 'Generic.kl' ! -name 'Vendor*' ! -name 'AVRCP*' ! -name 'qwerty*' ! -name 'sec_jack*' ! -name 's3c-k*' \\) -print0 | xargs -0 sed -i 's/^key \\([[:digit:]]*\\)\\([[:blank:]]*\\)\\(SEARCH\\)\\([[:blank:]]*\\)\\(VIRTUAL\\)/#key \1\2\3\4\5/'";
-				//String commentCmd = "find /sdcard/ -type f \\( -name \'*.kl\' ! -name \'Generic.kl\' ! -name \'Vendor*\' ! -name \'AVRCP*\' ! -name \'qwerty*\' ! -name \'sec_jack*\' ! -name \'s3c-k*\' \\) -print0 | xargs -0 sed -i \'s/^key \\([[:digit:]]*\\)\\([[:blank:]]*\\)\\(SEARCH\\)\\([[:blank:]]*\\)\\(VIRTUAL\\)/# key \1\2\3\4\5/\'";
-				String commentCmd = "";
-				BufferedReader br = null;
-			    try {
-			        br = new BufferedReader(new InputStreamReader(getAssets().open("comment.txt"))); //throwing a FileNotFoundException?
-			        String word = "";
-			        while((word=br.readLine()) != null)
-			        	commentCmd = word; //break txt file into different words, add to wordList
-			    }
-			        catch(IOException e) {
-			            e.printStackTrace();
-			        }
-			        finally {
-			            try {
-			                br.close(); //stop reading
-			            }
-			            catch(IOException ex) {
-			                ex.printStackTrace();
-			            }
-			        }
-				List<String> uc = RootTools.sendShell(commentCmd, 1000);
-				for(int i=0 ; i < uc.size(); i++)
-				{
-					Toast.makeText(getBaseContext(), i + " = " + uc.get(i), Toast.LENGTH_SHORT).show();
-				}
-				List<String> resetmntRes = RootTools.sendShell("mount -o ro,remount -t yaffs2 " + mntRes.get(0) + " /system/", 1000);
-				if(resetmntRes.size() == 1 && resetmntRes.get(0).equals("0"))
-				{
-					Toast.makeText(getBaseContext(), "Success: Mounted /system as RO.", Toast.LENGTH_SHORT).show();
-				}
-				else
-				{
-					Toast.makeText(getBaseContext(), "Failure: Could not mount /system as RO.\nError Code = " + resetmntRes.get(0), Toast.LENGTH_SHORT).show();
-					return;
-				}
-				Toast.makeText(getBaseContext(), "FIX SUCCESS", Toast.LENGTH_SHORT).show();
-				/*for(int i=0 ; i < remntRes.size(); i++)
-				{
-					Toast.makeText(getBaseContext(), i + " = " + remntRes.get(i), Toast.LENGTH_SHORT).show();
-				}*/
-			/*File sdCard = Environment.getExternalStorageDirectory();
-			File x = new File(sdCard.getAbsolutePath() + "/MyFiles");
-			String[] cmds = { "cd /", "tar zxvf /sdcard/MyFiles/com.amazon.venezia.tar.gz"};
-			Toast.makeText(getBaseContext(), "before", Toast.LENGTH_SHORT).show();
-			List<String> res = RootTools.sendShell(cmds, 50, 500);			
-			for(String x1 : res)
-			{
-				Toast.makeText(getBaseContext(), "tar result = " + x1, Toast.LENGTH_SHORT).show();
-			}
-			Toast.makeText(getBaseContext(), "result length" + res.size(), Toast.LENGTH_SHORT).show();
-			*/
-			}
-			catch(Exception e){e.printStackTrace();}
-			
+			fixCaptivateSearchButton(0); // 0 = Fix
 		}
 	};
 	
@@ -144,84 +186,7 @@ public class CaptivateSearchFixActivity extends Activity {
 		
 		@Override
 		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			String buttonTag = "";
-			if(optSearch.isChecked()) buttonTag = "SEARCH";
-			else if(optHome.isChecked()) buttonTag = "HOME";
-			else if(optMenu.isChecked()) buttonTag = "MENU";
-			else if(optBack.isChecked()) buttonTag = "BACK";
-			
-			try {
-			/*
-			 * TODO:
-			 * 1) Figure out how to tweak timeout and sleeptime values to get the toasts to display.
-			 * 2) See the RootTools wiki for instructions of "memory saving" method to do all superuser operations in just
-			 *    one RootTools code segment so that
-			 *    a) The superuser toast is not shown every time
-			 *    b) Hoping it has something to do with the toasts not being displayed below...and using "memory saving" method might fix it.
-			 */				
-				//get mount point
-				List<String> mntRes = RootTools.sendShell("mount | grep /system | sed -e 's/\\(.*\\) \\/system .*/\\1/'", 1000);
-				if(mntRes.size() == 2 && mntRes.get(1).equals("0"))
-				{
-					Toast.makeText(getBaseContext(), "Success. /system = " + mntRes.get(0), Toast.LENGTH_SHORT).show();
-				}
-				else
-				{
-					Toast.makeText(getBaseContext(), "Failure. Could not find /system. Value = " + mntRes.get(0) + " Error Code = " + mntRes.get(1), Toast.LENGTH_SHORT).show();
-					return;
-				}			
-				//Mount as rewritable
-				List<String> remntRes = RootTools.sendShell("mount -o rw,remount -t yaffs2 " + mntRes.get(0) + " /system/", 1000);
-				if(remntRes.size() == 1 && remntRes.get(0).equals("0"))
-				{
-					Toast.makeText(getBaseContext(), "Success: Mounted /system as RW.", Toast.LENGTH_SHORT).show();
-				}
-				else
-				{
-					Toast.makeText(getBaseContext(), "Failure: Could not mount /system as RW.\nError Code = " + remntRes.get(0), Toast.LENGTH_SHORT).show();
-					return;
-				}
-				//String unCommentCmd = "find /system/usr/keylayout/ -type f \\( -name '*.kl' ! -name 'Generic.kl' ! -name 'Vendor*' ! -name 'AVRCP*' ! -name 'qwerty*' ! -name 'sec_jack*' ! -name 's3c-k*' \\) -print0 | xargs -0 sed -i 's/^#[[:blank:]]*key \\([[:digit:]]*\\)\\([[:blank:]]*\\)\\(SEARCH\\)\\([[:blank:]]*\\)\\(VIRTUAL\\)/key \1\2\3\4\5/'";
-				//String unCommentCmd = "find /sdcard/ -type f \\( -name \'*.kl\' ! -name \'Generic.kl\' ! -name \'Vendor*\' ! -name \'AVRCP*\' ! -name \'qwerty*\' ! -name \'sec_jack*\' ! -name \'s3c-k*\' \\) -print0 | xargs -0 sed -i \'s/^#[[:blank:]]*key \\([[:digit:]]*\\)\\([[:blank:]]*\\)\\(SEARCH\\)\\([[:blank:]]*\\)\\(VIRTUAL\\)/key \1\2\3\4\5/\'";
-				String unCommentCmd = "";
-				BufferedReader br = null;
-			    try {
-			        br = new BufferedReader(new InputStreamReader(getAssets().open("uncomment.txt"))); //throwing a FileNotFoundException?
-			        String word = "";
-			        while((word=br.readLine()) != null)
-			        	unCommentCmd = word; //break txt file into different words, add to wordList
-			    }
-			        catch(IOException e) {
-			            e.printStackTrace();
-			        }
-			        finally {
-			            try {
-			                br.close(); //stop reading
-			            }
-			            catch(IOException ex) {
-			                ex.printStackTrace();
-			            }
-			        }
-				List<String> uc = RootTools.sendShell(unCommentCmd, 1000);
-				for(int i=0 ; i < uc.size(); i++)
-				{
-					Toast.makeText(getBaseContext(), i + " = " + uc.get(i), Toast.LENGTH_SHORT).show();
-				}
-				List<String> resetmntRes = RootTools.sendShell("mount -o ro,remount -t yaffs2 " + mntRes.get(0) + " /system/", 1000);
-				if(resetmntRes.size() == 1 && resetmntRes.get(0).equals("0"))
-				{
-					Toast.makeText(getBaseContext(), "Success: Mounted /system as RO.", Toast.LENGTH_SHORT).show();
-				}
-				else
-				{
-					Toast.makeText(getBaseContext(), "Failure: Could not mount /system as RO.\nError Code = " + resetmntRes.get(0), Toast.LENGTH_SHORT).show();
-					return;
-				}
-				Toast.makeText(getBaseContext(), "UNFIX SUCCESS", Toast.LENGTH_SHORT).show();
-			}
-			catch(Exception e){e.printStackTrace();}
-			
+			fixCaptivateSearchButton(1); // 1 = Unfix
 		}
 	};
 }
